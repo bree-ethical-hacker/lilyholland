20eed9a99f0334fc190b3b1fe420f82bdf6fe9e6
bree-ethical-hacker <kagwe.bridgette@gmail.com>
2025-12-24 19:47:43 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
-$('.image').ripples('show');
+$(document).ready(function () {
+  $('.image').ripples({
+    resolution: 256,
+    dropRadius: 20,
+    perturbance: 0.04
+  });
 });
